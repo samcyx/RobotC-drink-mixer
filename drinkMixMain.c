0a10934e109d3acba67c96f66f62edd032406f74
Sam <70551337+samcyx@users.noreply.github.com>
2024-11-01 12:55:48 -0400

    added main file

---
+++ drinkMixMain.c
@@ -0,0 +1,95 @@
+bool taskLiquid = true, taskPowder= true, taskStir = true;
+typedef struct {
+    byte curSpot;
+    int time;
+} Cup;
+
+
+task liquid()//Lucas
+{}
+
+task powder()//Henon
+{}
+
+task stir()//Devin
+{}
+
+void configureAllSensors()
+{
+	SensorType[S1] = sensorEV3_Color;
+  SensorMode[S1] = modeEV3Color_Reflected;
+	clearTimer(T1);
+
+}
+void finishedCup()//polls for whether the cup has been removed or not
+{
+
+}
+void rotate()//Free
+{
+
+}
+
+
+task main()
+{
+	short cupCount = 0;
+	bool finishedCup = false;
+	int lightThreshold;
+	int count = 0;
+
+	Cup cups[4];
+	configureAllSensors();
+	for(int i = 0; i < 4; i++)
+	{
+			Cup c;
+			c.curSpot = -1;
+
+			cups[i] = c;
+	}
+	while(!getButtonPress(ButtonEnter))
+	{
+		if(SensorValue(S1) > lightThreshold)//if we detect reflected light
+		{
+			Cup c;
+			c.curSpot = 0;
+
+			cups[count%4]	= c;
+			displayBigTextLine(4,"Press any button to start/continue operation");
+			while(!getButtonPress(buttonAny)){}
+			while(getButtonPress(buttonAny)){}
+			cups[count%4].time = timer1(T1);
+		}
+		count++;
+		for(int i = 0; i < 4; i++)
+		{
+				if(cups[i] == -1)
+					continue;
+				else if (cups[i].curSpot == 1)//cup at liquid station
+					taskLiquid();
+				else if(cups[i].curSpot  == 2)//cup at powder station
+					taskPowder();
+				else if(cups[i].curSpot  == 3) //cup at stirring station
+					taskStir();
+				else if(cups[i].curSpot == 4)//cup is at loading station
+				{
+					finishedCup == true;
+					cups[i].curSpot = -1;
+					cupCount++;
+					receiptPrint(cupCount,cups[i].curTime);
+					continue;
+				}
+				cups[i].curSpot++;
+		}
+		while(!((taskLiquid && taskPowder) && taskStir))//while the tasks are false
+		{}
+		stopAllTasks();
+		if(finishedCup)
+			finishedCup();//TODO - figure out how the removal of a cup will work -> another task?
+
+
+
+		rotate();
+	}
+
+}
