e15cb29f6eced43f7f23419bdb8725ad48ef214a
Sam <70551337+samcyx@users.noreply.github.com>
2024-11-04 16:44:52 -0500

    updated stir code with Devin's code

--- drinkMixMain.c
+++ drinkMixMain.c
@@ -14,8 +14,22 @@ typedef struct {
 } Cup;
 
 
-task liquid()//Lucas
-{}
+task stir()//Devin
+{
+	taskStir = false;
+	nMotorEncoder[motorA] = 0;
+	motor[motorA] = -50;
+	while(abs(nMotorEncoder[motorA])<(360*18))
+	{}
+	motor[motorA] = 0;
+	wait1Msec(100);
+	motor[motorA] = 50;
+	nMotorEncoder[motorA] = 0;
+	while(abs(nMotorEncoder[motorA])<(360*18))
+	{}
+	motor[motorA] = 0;
+	taskStir = true;
+}
 
 task powder()//Henon
 {}
