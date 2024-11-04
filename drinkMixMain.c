a7deb64bd63670adb896086142117e6750452857
Sam <70551337+samcyx@users.noreply.github.com>
2024-11-04 16:50:12 -0500

    added lucas's code

--- drinkMixMain.c
+++ drinkMixMain.c
@@ -1,11 +1,28 @@
 bool taskLiquid = true, taskPowder= true, taskStir = true;
 bool finishedCupRemoved = false;
 short lightThreshold;
+#include "PC_FileIO.c"
 int lightDetected(short threshold)
 {
 	if(SensorValue[S3] > threshold)
 		return true;
 	return false;
+}
+
+TFileHandle reciept;
+bool fileOkay = openWritePC(reciept, "reciept.txt");
+
+void recieptPrint (short cupCount, int time)
+{
+	string cup = "cup ";
+
+	writeTextPC(reciept, cup);
+
+	writeLongPC(reciept, cupCount);
+
+	writeLongPC{reciept, time);
+
+	writeEndlPC(reciept);
 }
 
 typedef struct {
@@ -14,8 +31,24 @@ typedef struct {
 } Cup;
 
 
-task liquid()//Lucas
-{}
+task liquid()
+{
+	taskLiquid = false;
+
+	const int LIQVOL = 250; //volume in mL
+	const float LIQCONV = //mL into encoder counts
+
+	nMotorEncoder(motorA) = 0;
+
+	motor[motorA] = 100;
+
+	while (abs(nMotorEncoder(motorA)) < LIQVOL * LIQCONV)
+	{}
+
+	motor[motorA] = 0;
+
+	taskLiquid = true;
+}
 
 task powder()//Henon
 {}
