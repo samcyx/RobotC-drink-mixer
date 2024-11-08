3ea3e04584ea3d16aed290ace4285a5ded70b0c7
HenonH <henonh1@gmail.com>
2024-11-07 22:11:49 -0500

    Update powderDispense.c

--- powderDispense.c
+++ powderDispense.c
@@ -1,1 +1,22 @@
+task powder() { 
+		finishCupRemoval = false;
+    clearTimer(timer3);
+    nMotorEncoder[motorC] = 0;
 
+
+    while (!finishCupRemoval) {}
+
+   
+    if (time1[timer3] >= time && SensorValue[S4] == 1) {
+        motor[motorC] = 10;   // Start motor
+        clearTimer(timer3);    // Reset timer3
+    }
+
+
+    while (nMotorEncoder[motorC] < 20 && time1[timer1] < time) {}
+
+    motor[motorC] = 0;  // Stop the motor
+  
+
+    turntableFunction();
+}
