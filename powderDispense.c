5e937c4a353b23e634b318898e599214f6bc4a93
HenonH <henonh1@gmail.com>
2024-11-11 22:52:09 -0500

    Update powderDispense.c

--- powderDispense.c
+++ powderDispense.c
@@ -1,13 +1,27 @@
-task powder() { 
+void taskPowder() { 
 	taskPowder = false;
-	nMotorEncoder[motorC] = 0;
-	wait1MS(1000);
+    	nMotorEncoder[motorC] = 0;
 	
-	motor[motorC] = 10;  //start mot
+    	const int HIGH_POWDER_LVL = 20;
+    	const int LOW_POWDER_LVL = 10;
+    	const int MEDIUM_POWDER_LVL = 15;
 
-   	while (nMotorEncoder[motorC] < 20 && time1[timer1] < time) {}
+    	int powderLVL;
+    	// Set encoder rotation levels based on powder level
+    	if (powderLevel == 1) {
+    	    powderLVL = HIGH_POWDER_LVL;
+    	} else if (powderLevel == 0) {
+    	    powderLVL = LOW_POWDER_LVL;
+    	} else {
+    		powderLVL = MEDIUM_POWDER_LVL;
+    	}
 
-    	motor[motorC] = 0;  // Stop the motor
-  	
-	taskPowder = true;
-	}
+   	// Start motor
+   	motor[motorC] = 20;  
+	wait1MS(50); //Slight delay to take load off CPU
+   	while (abs(nMotorEncoder[motorC]) < powderLVL) {}
+	motor[motorC] = 0;  // Stop the motor
+
+	
+   	taskPowder = true;
+}
